9dbfdf1c3871f968809d12f90054132df70d13d4
phamminhteky1 <186171919+phamminhteky1-fix@users.noreply.github.com>
2025-10-15 15:31:24 +0700

    Initial frontend commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>This is an app that I'm working on, so nothin here.</center>
+    </div>
+  );
+}
+
+export default App;
