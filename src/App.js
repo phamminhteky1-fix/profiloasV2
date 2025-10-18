8a136c26820eb8597e12792c0d9d6db173e21bc1
phamminhteky1 <186171919+phamminhteky1-fix@users.noreply.github.com>
2025-10-18 21:43:20 +0700

    Update too small too talk about

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1><center>This is an app that I'm working on, so nothin here.</center></h1>
+      <center><p>In the meantime, please check out my main web: https://phamminhteky1-fix.github.io</p></center>
     </div>
   );
 }
